b3ab49432831dcc247d800a77eb2cdc9fd1a8d8d
pkonduru <praveen.konduru@gmail.com>
2021-08-06 10:59:45 -0500

    Update Factorial.java
    Looks like the fibonacci not factorial updated the definition

--- src/main/java/com/spsa/algo/warmup/Factorial.java
+++ src/main/java/com/spsa/algo/warmup/Factorial.java
@@ -2,9 +2,9 @@ package com.spsa.algo.warmup;
 
 import org.junit.Assert;
 
-public class Factorial {
+public class Fibonacci {
                                                  // 0 1 2 3 4 5 6
-    public static int factorialBottomUp(int n) { // 1 1 2 3 5 8 13
+    public static int fibonacciBottomUp(int n) { // 1 1 2 3 5 8 13
         int a = 1;
         int b = 1;
         for (int i = 2; i <= n; i++) { 
@@ -16,9 +16,9 @@ public class Factorial {
         return b;
     }
     public static void main(String[] args) {
-        Assert.assertEquals(5, factorialBottomUp(4));
-        Assert.assertEquals(8, factorialBottomUp(5));
-        Assert.assertEquals(13, factorialBottomUp(6));
+        Assert.assertEquals(5, fibonacciBottomUp(4));
+        Assert.assertEquals(8, fibonacciBottomUp(5));
+        Assert.assertEquals(13, fibonacciBottomUp(6));
     }
 }
 
